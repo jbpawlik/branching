d1102463d797a06b7cc363fe1be1fb9dcc4d5e06
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-02 16:04:32 -0700

    add HTML, scripts.js, and styles.css

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  const age = parseInt(prompt("How old are you?"));
+
+
+  if 
+    (age === 21) {
+    $('#bday').show(); 
+  } else if
+    (age > 21) {
+    $('#drinks').show();
+  } else 
+    $('#under-21').show();
+});
